67bda62fe07ac08f836c3f8fbde7134ac73f3153
Triksheim <martin.riksheim@outlook.com>
2024-04-10 18:07:01 +0200

    Branching simulation changes and improvements. Functionailty to convert result into map layers with correct CRS.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from utility import *
 from geo_services import *
+from SAR_model_functions import *
+
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from scipy.ndimage import uniform_filter, generic_filter, maximum_filter
 from matplotlib.patches import Circle
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import geopandas as gpd
 
 
 
@@ -25,8 +30,8 @@ def get_height_map(center_of_map_squares, start_coords=("00.00","00.00"), rect_r
     """
     
     # serivce 1 - DTM
-    #coverage_identifier = "nhm_dtm_topo_25833"
-    #url = "https://wcs.geonorge.no/skwms1/wcs.hoyde-dtm-nhm-25833"
+    # coverage_identifier = "nhm_dtm_topo_25833"
+    # url = "https://wcs.geonorge.no/skwms1/wcs.hoyde-dtm-nhm-25833"
 
     # service 2 - DOM
     coverage_identifier = "nhm_dom_25833"
@@ -41,6 +46,7 @@ def get_height_map(center_of_map_squares, start_coords=("00.00","00.00"), rect_r
     with ThreadPoolExecutor() as executor:
         print(f'Request to WCS: {url}')
         for coords in center_of_map_squares:
+            time.sleep(5)
             bbox_coords = calculate_bbox(coords[0], coords[1], rect_radius)
             bbox = create_bbox_string(*bbox_coords)
 
@@ -117,6 +123,7 @@ def get_terrain_type_map(center_for_map_squares, start_coords=("00.00","00.00"),
             
             future = executor.submit(wms_request, url, params)
             futures.append(future)
+            
 
         for n, future in enumerate(as_completed(futures)):
             response = future.result()
@@ -239,13 +246,13 @@ def terrain_encoding(terrain_filename="terrain_RGB_data.npy", trails_filename="t
     terrain_encoding = {
         "Ukjent":       1,
         "Sti og vei":   1, 
-        "Åpen fastmark":0.7,
-        "Bebygd":       0.7,
+        "Åpen fastmark":0.8,
+        "Bebygd":       0.8,
         "Dyrket mark":  0.6, 
-        "Skog":         0.5,
+        "Skog":         0.6,
         "Myr":          0.3,
-        "Ferskvann":    0.1,
-        "Hav":          0.05,   
+        "Ferskvann":    0.05,
+        "Hav":          0.01,   
     }
 
     # create a new 2d array with the terrain type encoding based on rgb values
@@ -344,7 +351,6 @@ def get_all_map_data(lat, lng, rect_radius=1000, map_extention=0, folder="output
         for x in range(-map_extention, map_extention+1, 1):
             center_of_map_squares.append((center_x + (2*x*rect_radius), center_y + (2*y*rect_radius)))
 
-
     # get the terrain type map (saves tiff file)
     get_terrain_type_map(center_of_map_squares, start_coords, rect_radius)
 
@@ -378,27 +384,44 @@ if __name__ == "__main__":
     ####################################################################################################
     plot = True
 
-    r = False   # Set to run all collection and preprocessing functions
-    collect = r or False
+
+    collect = True
+
+    r = True # Set to run all preprocessing functions
     encode = r or False
     reduced_resolution = r or False
     calculate_steepness = r or False
     combine_matrix = r or False
     smoothed_matrix = r or False
 
-    straight_line_test = False
+    straight_line_test = True
     branching_line_test = True
+
+    create_map_overlay = True
     
     ####################################################################################################
-    
-    
+    #67.31458155986105, 14.477247863863925  keiservarden
+    #68.44333623319275, 17.52796511156201   pumpvannet
+    #68.26615067072053, 14.537723823348557  fra prototype
+
+    start_lat = 68.26615
+    start_lng = 14.53772
+
+    square_radius = 500     # meter radius from center, total 2*r x 2*r area
+    map_extention = 0       # extends from center point by 2*map_extention*square_radius in each direction
 
     if collect:
-        start_lat = 68.443440
-        start_lng = 17.527820
-        rect_radius = 500 # meter radius from center, total 2*r x 2*r area
-        map_extention = 0   # extends from center point by 2*map_extention*rect_radius in each direction
-        get_all_map_data(start_lat, start_lng, rect_radius, map_extention)
+        get_all_map_data(start_lat, start_lng, square_radius, map_extention)
+
+        if plot:
+            height_data = np.load("output/array/height_data.npy")
+            plot_array(height_data, cmap='terrain', label="moh", title="Høydedata")
+
+            try:
+                trail_data = np.load("output/array/trail_data.npy")
+                plot_array(trail_data, cmap='terrain', title="Stier")
+            except FileNotFoundError:
+                print("No trail data found")
 
 
     if encode:
@@ -408,7 +431,7 @@ if __name__ == "__main__":
         terrain_encoding(filename1, filename2, output)
         if plot:
             terrain_data = np.load("output/array/terrain_data_encoded.npy")
-            plot_array(terrain_data, cmap='terrain', label="Terreng")
+            plot_array(terrain_data, cmap='terrain', label="Verdi(0-1)", title="Arealtype")
     
 
     if reduced_resolution:
@@ -427,17 +450,18 @@ if __name__ == "__main__":
             height_data = height_data[:-1,:-1]
 
         steepness_map = calc_steepness(height_data)
-        #print(steepness_map.shape)
         
-        steepness_map[steepness_map > 25] = 25  # Cap the steepness at 25%
+        
+        
+        steepness_map[steepness_map > 10] = 10  # Cap steepness value at 10
         normalized_steepness_map = steepness_map / np.max(steepness_map)
         normalized_steepness_map = 1 - normalized_steepness_map # Invert the steepness
         normalized_steepness_map = normalized_steepness_map ** 2
-        #print(normalized_steepness_map.shape)
+        
 
         if plot:
-            plot_array(steepness_map, cmap='terrain', label="Stigning (%)")
-            plot_array(normalized_steepness_map, cmap='terrain', label="Normalisert stigning (0-1)")
+            plot_array(steepness_map, cmap='terrain', label="Gradering", title="Normalisert Stigning")
+            plot_array(normalized_steepness_map, cmap='terrain', label="Bratt  ->  Flatt", title="Invers normalisert stigning")
         
         np.save("output/array/normalized_steepness_map.npy", normalized_steepness_map)
 
@@ -448,7 +472,18 @@ if __name__ == "__main__":
         if not calculate_steepness:
             normalized_steepness_map = np.load("output/array/normalized_steepness_map.npy")
 
-        combined_matrix = combine_matrixes(terrain_data, normalized_steepness_map, method="square")
+
+        #combined_matrix = combine_matrixes(terrain_data, normalized_steepness_map, method="square")
+
+        new_steepness_map = normalized_steepness_map
+        new_steepness_map[terrain_data == 1] = 1
+
+        new_steepness_map[new_steepness_map <= 0.5] = 0
+
+        new_terrain_map = terrain_data
+        new_terrain_map[new_terrain_map <= 0.05] = 0
+
+        combined_matrix = combine_matrixes(new_terrain_map, new_steepness_map, method="square")
 
         if smoothed_matrix:
             filter_size = 3     # nxn filter size
@@ -456,10 +491,8 @@ if __name__ == "__main__":
             combined_matrix = maximum_filter(combined_matrix, size=filter_size)
 
         if plot:
-            plt.imshow(combined_matrix, cmap='terrain', interpolation='nearest')
-            plt.axis('equal')
-            plt.title("Combined matrix of height and terrain (0-1)")
-            plt.show()
+            plot_array(combined_matrix, cmap='terrain', label="Vanskelig  ->  Lett", title="Kombinert matrise, terreng score")
+            
 
         np.save("output/array/combined_matrix.npy", combined_matrix)
 
@@ -479,7 +512,7 @@ if __name__ == "__main__":
         
         radius = combined_matrix.shape[0] / 2
         center = (int(radius), int(radius))
-        dist_range_factor = 1.25
+        dist_range_factor = 2.5
         max_distance = radius * dist_range_factor
        
         end_points_75 = []
@@ -525,7 +558,7 @@ if __name__ == "__main__":
 
             x_coords, y_coords = zip(*end_points_75)
             plt.scatter(y_coords, x_coords, color='green', s=5)
-
+            
             plt.axis('equal')
             plt.title("Straight line heatmap")
             plt.show()
@@ -546,20 +579,50 @@ if __name__ == "__main__":
         
         radius = combined_matrix.shape[0] / 2
         center = (int(radius), int(radius))
-        dist_range_factor = 2.5     # deterimines "max" travel distance
+        dist_range_factor = 2.5    # deterimines "max" travel distance
         max_distance = radius * dist_range_factor
         max_energy = max_distance
-        alpha = 7  # "concavity" of the search area hull
+        alpha = 25  # "concavity" of the search area hull
+
+        
 
         green_coords = set()
         yellow_coords = set()
         red_coords = set()
-        sets = (green_coords, yellow_coords, red_coords)
+        branches = set()
+        
+        sets = (green_coords, yellow_coords, red_coords, branches)
 
-        for initial_angle in range(360):
-            branching(combined_matrix, center[0], center[1], initial_angle, max_energy, max_energy, sets)
+        print("Branching simulation started...")
 
-        print(len(red_coords))  # number of endpoints/paths
+
+
+        # old function
+        # for initial_angle in range(360):
+        #    branching(combined_matrix, center[0], center[1], initial_angle, max_energy, max_energy, sets)
+
+
+
+        iterations = 1
+        directions = 8
+        total = iterations * directions
+        
+        for n in range(iterations):
+            curr_dir = 1
+            for i in range(-1, 2, 1):
+                for j in range(-1, 2, 1):
+                    move_direction = (i, j)
+                    if move_direction != (0, 0):
+                        print(f'Iteration {n+1}/{iterations}, Direction {curr_dir}/{directions}')
+                        curr_dir += 1
+                        branches = set() # reset branches
+                        sets = (green_coords, yellow_coords, red_coords, branches)
+                        branching_movement(combined_matrix, (center[0], center[1]), move_direction, max_energy, max_energy, sets)
+                        
+
+        print(f'Unique paths simulated: {len(red_coords)}')  # number of endpoints/paths
+        
+        debug_stats_print()
 
         # convert sets to np arrays
         red_points = np.array(list(red_coords))
@@ -574,25 +637,48 @@ if __name__ == "__main__":
 
         if plot:
             plt.imshow(combined_matrix, cmap='terrain', interpolation='nearest')
+            plt.colorbar(label="Terreng: Vaskelig  ->  Lett")
             circle = Circle((radius, radius), (radius-(radius/10)), color="blue", fill=False)   # search area circle
             plt.gca().add_patch(circle)
 
             if concave_hull_r:
-                x_r, y_r = get_polygon_from_hull(concave_hull_r)
+                x_r, y_r = get_polygon_coords_from_hull(concave_hull_r)
                 plt.fill(x_r, y_r, edgecolor='r',linewidth=3, fill=False)
             if concave_hull_y:
-                x_y, y_y = get_polygon_from_hull(concave_hull_y)
+                x_y, y_y = get_polygon_coords_from_hull(concave_hull_y)
                 plt.fill(x_y, y_y, edgecolor='y',linewidth=3, fill=False)
             if concave_hull_g:
-                x_g, y_g = get_polygon_from_hull(concave_hull_g)
+                x_g, y_g = get_polygon_coords_from_hull(concave_hull_g)
                 plt.fill(x_g, y_g, edgecolor='g',linewidth=3, fill=False)
 
+            # plot endpoints
+            #plt.scatter(red_points[:,0], red_points[:,1], color='red', s=5)
+            #plt.scatter(yellow_points[:,0], yellow_points[:,1], color='yellow', s=25)
+            #plt.scatter(green_points[:,0], green_points[:,1], color='green', s=50)
+
+            
+            plt.title("Branching result plot")
             plt.axis('equal')
+            
             plt.show()
     
 
 
+    if create_map_overlay:
+
+        start_coords = start_lat, start_lng  # matrix center
+        map_diameter = ((map_extention * 2) + 1) * (square_radius * 2)  # Diameter of the map in meters
+        distance_per_index =  map_diameter / combined_matrix.shape[0]  # Meters per index in the matrix
+
+        # create map overlays for red, yellow and green areas
+        create_polygon_map_overlay(combined_matrix, distance_per_index, start_coords, concave_hull_r, color="red")
+        create_polygon_map_overlay(combined_matrix, distance_per_index, start_coords, concave_hull_y, color="yellow")
+        create_polygon_map_overlay(combined_matrix, distance_per_index, start_coords, concave_hull_g, color="green")
 
 
+
+
+
+        
 
     
